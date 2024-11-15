c3553e28be305c014c911dfa4310eecf4cc0c0bc
osva1do <omtzm00@gmail.com>
2024-11-14 18:19:00 -0600

    actualizacion de cors

--- configs/cors.js
+++ configs/cors.js
@@ -2,9 +2,7 @@ import cors from "cors";
 
 const ORIGINS = [
   "http://localhost:5173",
-  "https://localhost:5173",
-  "http://rhino-labs.com",
-  "https://rhino-labs.com",
+  "https://rino-labs.vercel.app/",
 ];
 
 export const corsMiddleware = ({ acceptedOrigins = ORIGINS } = {}) =>
